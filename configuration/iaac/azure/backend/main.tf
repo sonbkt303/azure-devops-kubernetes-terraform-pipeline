a6b7d7a38810d44e806e6b0caa1ea0c38afa6bc2
mike bui <mike.bui.0031>
2024-03-04 14:36:59 +0700

    Update

--- configuration/iaac/azure/backend/main.tf
+++ configuration/iaac/azure/backend/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+
 provider "azurerm" {
   # version = "~> 1.42"
   client_id       = var.client_id   # ENVIRONMENT VARIABLE
