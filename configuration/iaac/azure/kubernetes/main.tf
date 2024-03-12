72647ddcff6dd768b9ddeebca71adc376dc47642
mike bui <mike.bui.0031>
2024-03-12 14:40:42 +0700

    Update

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -33,11 +33,17 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   }
 }
 
-terraform {
-  backend "azurerm" {
-    # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
-    # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
-    # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
-    # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
-  }
+provider "azurerm" {
+  features {}
+  subscription_id   = "Azure subscription(44e3fb22-c762-4f5e-b485-f6b13f45f3bd)"
+  alias             = "Test-001"
 }
+
+# terraform {
+#   backend "azurerm" {
+#     # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
+#     # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
+#     # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
+#     # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
+#   }
+# }
