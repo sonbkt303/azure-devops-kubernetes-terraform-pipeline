cf8fd9eff07eb564ee16e2a6930002eda82cf3fc
mike bui <mike.bui.0031>
2024-03-12 15:01:03 +0700

    update end this stage not work

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -33,26 +33,26 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   }
 }
 
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.95.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {
-    
-  }
-}
-
 # terraform {
-#   backend "azurerm" {
-#     # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
-#     # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
-#     # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
-#     # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
+#   required_providers {
+#     azurerm = {
+#       source = "hashicorp/azurerm"
+#       version = "3.95.0"
+#     }
 #   }
 # }
+
+# provider "azurerm" {
+#   features {
+    
+#   }
+# }
+
+terraform {
+  backend "azurerm" {
+    # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
+    # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
+    # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
+    # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
+  }
+}
